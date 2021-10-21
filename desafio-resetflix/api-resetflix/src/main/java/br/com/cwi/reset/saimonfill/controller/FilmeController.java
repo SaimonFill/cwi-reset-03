9d621bc9fd7b9c0deea538db808326db74bf3135
Saimon Gabriel Fill <Saimon@coester.com.br>
2021-10-21 09:05:57 -0300

    Implementa classe FilmeService e FilmeController.

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/saimonfill/controller/FilmeController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/saimonfill/controller/FilmeController.java
@@ -1,4 +1,28 @@
 package br.com.cwi.reset.saimonfill.controller;
 
+import br.com.cwi.reset.saimonfill.FakeDatabase;
+import br.com.cwi.reset.saimonfill.request.FilmeRequest;
+import br.com.cwi.reset.saimonfill.service.FilmeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 public class FilmeController {
+
+    @RestController
+    @RequestMapping("/filmes")
+    public class FilmeController {
+
+        private FilmeService filmeService;
+
+        public FilmeController() {
+            this.filmeService = new FilmeService((FakeDatabase.getInstance()));
+        }
+
+        @PostMapping
+        @ResponseStatus(HttpStatus.CREATED)
+        public void criarFilme(@RequestBody FilmeRequest filmeRequest) throws Exception {
+            this.filmeService.criarFilme(filmeRequest);
+
+        }
+    }
 }
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/saimonfill/service/FilmeService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/saimonfill/service/FilmeService.java
@@ -1,4 +1,48 @@
 package br.com.cwi.reset.saimonfill.service;
 
+import br.com.cwi.reset.saimonfill.FakeDatabase;
+import br.com.cwi.reset.saimonfill.model.Diretor;
+import br.com.cwi.reset.saimonfill.model.Estudio;
+import br.com.cwi.reset.saimonfill.model.Filme;
+import br.com.cwi.reset.saimonfill.model.PersonagemAtor;
+import br.com.cwi.reset.saimonfill.request.FilmeRequest;
+
+import java.util.List;
+
 public class FilmeService {
+
+    private FakeDatabase fakeDatabase;
+    private DiretorService diretorService;
+    private EstudioService estudioService;
+    private PersonagemService personagemService;
+
+    public FilmeService(FakeDatabase fakeDatabase) {
+        this.fakeDatabase = fakeDatabase;
+        this.diretorService = new DiretorService(FakeDatabase.getInstance());
+        this.estudioService = new EstudioService((FakeDatabase.getInstance()));
+        this.personagemService = new PersonagemService((FakeDatabase.getInstance()));
+    }
+
+    public void criarFilme(FilmeRequest filmeRequest) throws Exception {
+
+        Diretor idDiretor = diretorService.consultarDiretor(filmeRequest.getIdDiretor());
+        Estudio idEstudio = estudioService.consultarEstudio(filmeRequest.getIdEstudio());
+        List<PersonagemAtor> personagens = personagemService.criarPersonagem(filmeRequest.getPersonagens());
+
+        Integer idGerado = fakeDatabase.recuperaFilmes().size() + 1;
+
+        Filme filme = new Filme(
+                idGerado,
+                filmeRequest.getNome(),
+                filmeRequest.getAnoLancamento(),
+                filmeRequest.getCapaFilme(),
+                filmeRequest.getGeneros(),
+                idDiretor,
+                idEstudio,
+                personagens
+        );
+
+        fakeDatabase.persisteFilme(filme);
+    }
+
 }
